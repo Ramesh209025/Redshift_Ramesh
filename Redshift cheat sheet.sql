f99a9539197f5dff08ad3512eb96c4976dd2ae6c
Ramesh <r091463@gmail.com>
2018-03-13 12:18:56 +0530

    Update Redshift cheat sheet.sql

--- Redshift cheat sheet.sql
+++ Redshift cheat sheet.sql
@@ -13,3 +13,36 @@ select pg_size_pretty(pg_relation_size(relid)) from pg_stat_user_tables where re
 
 -- command line, dump table
 pg_dump -h hostname -p port -d dbname -U username -s (schema only) -t tablename
+
+
+
+
+## General Stuff
+
+### List of all tables
+SELECT * FROM pg_catalog.pg_tables
+
+### Create new user and give it superuser access
+create user adminuser createuser password '1234Admin';
+alter user adminuser createuser;
+
+### Create user without superuser privilege
+select md5('password' || 'username'); -- Generate the md5 for the password
+create user user1 password 'md537af65b44378ac7a5a1fb187a1969c71'; -- Use the generated md5 as password
+
+### Grant all to specific user
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO user;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO user;
+
+### Drop User
+drop user danny;
+
+### Revoke database access from a user
+revoke all on database dwtable from dwuser;
+
+### Change user password
+alter user newuser password 'EXAMPLENewPassword11'; 
+
+### Automatically gives access when new table is created
+ALTER DEFAULT PRIVILEGES GRANT SELECT ON TABLES TO PUBLIC
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO PUBLIC
